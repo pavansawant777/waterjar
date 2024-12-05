1f8ffe9aea2e7b33c3a23fe0ab3e3692e64da725
pavansawant777 <126850815+pavansawant777@users.noreply.github.com>
2024-12-05 17:52:50 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost','root','','waterjar_project');
+$conn = mysqli_connect('b6dhbiuom8gzftauydjq-mysql.services.clever-cloud.com','utpuml3fcdt01e94','Bcg11C5XmPG7BsLK6oed','b6dhbiuom8gzftauydjq');
 ?>
